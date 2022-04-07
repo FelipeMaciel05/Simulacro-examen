354cca7503700c6a2d6018d7380e58f4b13413da
FelipeMaciel05 <felimorita05@gmail.com>
2022-04-07 15:09:01 -0300

    2 ejercicios terminados

---
+++ Assets/PrimerEjercicio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimerEjercicio : MonoBehaviour
+{
+    //1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+
+
+    public int n1;
+    public int n2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (n1 == n2)
+        {
+            Debug.Log("tu numero es igual");
+        }
+        else
+        {
+            Debug.Log("tu numero no es igual, vuelve a ingresarlos");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SegundoEjercicio.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegundoEjercicio : MonoBehaviour
+{
+    //2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible.Luego indicar cuánto dinero sobra o falta.
+
+    public int precio1;
+    public int precio2;
+    public int precio3;
+    public int montototal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int sumadeprecios;
+        int sobra;
+        int falta;
+        sumadeprecios = precio1 + precio2 + precio3;
+        if (sumadeprecios < montototal)
+        {
+            Debug.Log("No supera el limite establecido");
+            sobra = montototal - sumadeprecios;
+            Debug.Log("Sobran " + sobra);
+        }
+        else
+        {
+            Debug.Log("supera el limite establecido");
+            falta = sumadeprecios - montototal;
+            Debug.Log("Faltan " + falta);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
